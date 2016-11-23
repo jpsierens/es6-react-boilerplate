644940571cead3350944ed9aeebc55bae328738b
David Nuñez <davidnunez001@hotmail.com>
2016-11-23 10:49:20 -0600

    Update SearchableTable.js
    Changing Spaces to Tabs.
    Adding commas.
    Adding a space at the end of a parenthesis ")".

--- app/SearchableTable.js
+++ app/SearchableTable.js
@@ -10,20 +10,20 @@ export default class SearchableTable extends React.Component {
 	constructor() {
 		super();
 		// Initial state of the component
-        this.state = {filterText: ''}
-    }
-    handleUserInput(filterText) {
-    	// When there's a change in the state, the component and all its 
-    	// sub-components get updated.
-        this.setState({filterText: filterText});
-    }
-	render(){
+		this.state = {filterText: ''};
+	}
+	handleUserInput(filterText) {
+		// When there's a change in the state, the component and all its 
+		// sub-components get updated.
+		this.setState({filterText: filterText});
+	}
+	render() {
 		return (
 			<div>
 				<SearchBar 
 					filterText={this.state.filterText}
-                    onUserInput={this.handleUserInput.bind(this)}
-                />
+					onUserInput={this.handleUserInput.bind(this)}
+				/>
 				<Table 
 					data={this.props.data} 
 					filterText={this.state.filterText}
@@ -36,45 +36,45 @@ export default class SearchableTable extends React.Component {
 class SearchBar extends React.Component {
 	handleChange() {
 		// passing filter data up by using a callback
-        this.props.onUserInput(
-        	// ref is like the id
-            this.refs.filterTextInput.value
-        );
-    }
-	render(){
+		this.props.onUserInput(
+			// ref is like the id
+			this.refs.filterTextInput.value
+		);
+	}
+	render() {
 		return (
-            <form>
-                <input 
-                	type="text" 
-                	placeholder="Search for one keyword..." 
-                	ref="filterTextInput"
-                	value= {this.props.filterText}
-                	onChange= {this.handleChange.bind(this)} 
-                />
-            </form>
-        );
+			<form>
+				<input 
+					type="text" 
+					placeholder="Search for one keyword..." 
+					ref="filterTextInput"
+					value= {this.props.filterText}
+					onChange= {this.handleChange.bind(this)} 
+				/>
+			</form>
+		);
 	}
 }
 
 class Table extends React.Component {
-	render(){
+	render() {
 		let sections = [];
 		let data = this.props.data;
-		data.forEach(function(product){
+		data.forEach(function(product) {
 			if (product.name.indexOf(this.props.filterText) === -1) {
 				return;
 			}
 			sections.push(<Section key={product.name} data={product} />);
-		}.bind(this))
-		return(
+		}.bind(this));
+		return (
 			<div>{sections}</div>
 		);
 	}
 }
 
 class Section extends React.Component {
-	render(){
-		return(
+	render() {
+		return (
 			<div>
 				<p>{this.props.data.name} = {this.props.data.price} </p>
 			</div>
